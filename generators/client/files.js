5d0fb74a47502b87b73a0f439996469dcbf8db2c
Angelo Manganiello <angelo.mang@libero.it>
2019-10-10 22:10:56 +0200

    fix entity field mandatory and react home page

--- generators/client/files.js
+++ generators/client/files.js
@@ -1,6 +1,7 @@
 const jhipsterConstants = require('generator-jhipster/generators/generator-constants');
 
 const ANGULAR_DIR = jhipsterConstants.ANGULAR_DIR;
+const REACT_DIR = jhipsterConstants.REACT_DIR;
 
 const clientFiles = {
     common: [
@@ -27,7 +28,19 @@ const clientFiles = {
                 {
                     file: 'angular/home/home.component.html',
                     method: 'processHtml',
-                    renameTo: () => `${ANGULAR_DIR}/home/home.component.html`
+                    renameTo: () => `${ANGULAR_DIR}home/home.component.html`
+                }
+            ]
+        }
+    ],
+    reactMain: [
+        {
+            condition: generator => generator.clientFramework === 'react',
+            templates: [
+                {
+                    file: 'react/home/home.tsx',
+                    method: 'processJsx',
+                    renameTo: () => `${REACT_DIR}modules/home/home.tsx`
                 }
             ]
         }
