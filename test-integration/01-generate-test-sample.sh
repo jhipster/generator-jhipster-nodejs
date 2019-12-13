6253331f7812c646dfb97ca677819c4870b17160
Angelo Manganiello <angelo.mang@libero.it>
2019-12-13 16:05:53 +0100

    added test templates for integration tests with sh files

---
+++ test-integration/01-generate-test-sample.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+set -e
+
+launchCurl() {
+    sleep 20
+    retryCount=1
+    maxRetry=10
+    httpUrl="http://localhost:8081/$1/management/health"
+    rep=$(curl -v "$httpUrl")
+    status=$?
+    while [ "$status" -ne 0 ] && [ "$retryCount" -le "$maxRetry" ]; do
+        echo "*** [$(date)] Application not reachable yet. Sleep and retry - retryCount =" $retryCount "/" $maxRetry
+        retryCount=$((retryCount+1))
+        sleep 10
+        rep=$(curl -v "$httpUrl")
+        status=$?
+    done
+
+    if [ "$status" -ne 0 ]; then
+        echo "*** [$(date)] Not connected after" $retryCount " retries."
+        exit 1
+    fi
+}
+
+runApp() {
+    npm start:app &
+    echo $! > .pidRunApp
+}
+
+
+#-------------------------------------------------------------------------------
+# Change in template directory
+#-------------------------------------------------------------------------------
+cd test-integration/samples/$1
+echo "*** changed directory in : test-integration/samples/"$1
+
+#-------------------------------------------------------------------------------
+# Run NHipster Generator
+#-------------------------------------------------------------------------------
+echo "*** run nodejs blueprint for : "$1
+jhipster --blueprints nodejs --skip-git --skip-checks --force --no-insight
+echo "*** install server dependencies for : "$1
+cd server && npm install
+
+#-------------------------------------------------------------------------------
+# Run and test app
+#-------------------------------------------------------------------------------
+echo "*** run app : "$1
+runApp
+launchCurl
+
+
+#-------------------------------------------------------------------------------
+# Kill app
+#-------------------------------------------------------------------------------
+echo "*** kill app : "$1
+kill $(cat .pidRunApp)
---
+++ test-integration/samples/README.md
@@ -0,0 +1,11 @@
+# Sample application generation
+
+To generate test applications, either automatically by the CI or locally on the developer machine, a number of pre-defined configurations have been prepared.
+
+## Application configurations
+
+Those are described in `.yo-rc.json` files which is the descriptor file created by Yeoman to keep track of the choices selected while generating an application.
+
+-   monolith-angular-jwt-dev
+-   monolith-react-oauth2-dev
+
