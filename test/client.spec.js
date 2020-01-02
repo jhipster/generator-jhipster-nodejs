0a9e41364a6fdbe7b474c425df4978241587e8af
Angelo Manganiello <angelo.mang@libero.it>
2020-01-02 20:05:34 +0100

    added sonar-scanner

--- test/client.spec.js
+++ test/client.spec.js
@@ -2,41 +2,63 @@ const path = require('path');
 const assert = require('yeoman-assert');
 const helpers = require('yeoman-test');
 
+// initial precondition for all tests
+function getPreCondition() {
+    return helpers
+        .run('generator-jhipster/generators/client')
+        .withOptions({
+            'from-cli': true,
+            skipInstall: true,
+            skipServer: true,
+            blueprints: 'nodejs',
+            skipChecks: true,
+            db: 'mysql',
+            auth: 'jwt'
+        })
+        .withGenerators([
+            [
+                require('../generators/client/index.js'), // eslint-disable-line global-require
+                'jhipster-nodejs:client',
+                path.join(__dirname, '../generators/client/index.js')
+            ]
+        ]);
+}
+
+const commonPrompt = {
+    baseName: 'sampleMysql',
+    clientTheme: 'none'
+};
+
 describe('Subgenerator client of nodejs JHipster blueprint', () => {
-    describe('1-test', () => {
+    describe('1-angular test', () => {
         before(done => {
-            helpers
-                .run('generator-jhipster/generators/client')
-                .withOptions({
-                    'from-cli': true,
-                    skipInstall: true,
-                    skipServer: true,
-                    blueprints: 'nodejs',
-                    skipChecks: true
-                })
-                .withGenerators([
-                    [
-                        require('../generators/client/index.js'), // eslint-disable-line global-require
-                        'jhipster-nodejs:client',
-                        path.join(__dirname, '../generators/client/index.js')
-                    ]
-                ])
+            getPreCondition()
                 .withPrompts({
-                    baseName: 'sampleMysql',
-                    applicationType: 'monolith',
-                    authenticationType: 'jwt',
-                    prodDatabaseType: 'mysql',
-                    enableTranslation: true,
-                    nativeLanguage: 'en',
-                    languages: ['fr', 'de'],
-                    rememberMeKey: '2bb60a80889aa6e6767e9ccd8714982681152aa5'
+                    commonPrompt,
+                    clientFramework: 'angularX'
                 })
                 .on('end', done);
         });
 
-        it('it works', () => {
+        it('angular app exists with custom package.json scripts', () => {
             // Adds your tests here
-            assert.textEqual('Write your own tests!', 'Write your own tests!');
+            assert.fileContent('package.json', '"start:app": "npm run build && cd server && npm run start"');
+        });
+    });
+
+    describe('2-react test', () => {
+        before(done => {
+            getPreCondition()
+                .withPrompts({
+                    commonPrompt,
+                    clientFramework: 'react'
+                })
+                .on('end', done);
+        });
+
+        it('react app exists with custom eslint rules', () => {
+            // Adds your tests here
+            assert.fileContent('.eslintrc.json', '"prefer-promise-reject-errors": "off"');
         });
     });
 });
