8b2f11a1313199a409de6e6f42372288c4d754a2
Angelo Manganiello <angelo.mang@libero.it>
2020-01-12 12:27:26 +0100

    fix controller usage

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,7 +1,10 @@
+<a name="1.0.0"></a>
 <a name="1.0.0-beta.2"></a>
 <a name="1.0.0-beta.1"></a>
 <a name="1.0.0-alpha.3"></a>
 <a name="1.0.0-alpha.2"></a>
+
+# [1.0.0](https://github.com/jhipster/generator-jhipster-nodejs/tree/v1.0.0)
 
 # [1.0.0-beta.2](https://github.com/jhipster/generator-jhipster-nodejs/tree/v1.0.0-beta.2)
 
--- test-integration/02-generate-component-sample.sh
+++ test-integration/02-generate-component-sample.sh
@@ -5,6 +5,7 @@ set -e
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 
+
 #-------------------------------------------------------------------------------
 # Change in template directory
 #-------------------------------------------------------------------------------
@@ -13,41 +14,22 @@ echo "*** changed directory in : test-integration/samples/"$1
 
 
 #-------------------------------------------------------------------------------
-# Run NHipster Generator
+# Generate component (service or controller)
+# $2 : service or controller
+# $3 : name
 #-------------------------------------------------------------------------------
-echo "*** run generation app with nodejs blueprint for : "$1
+echo "*** run generation" $2 $3 "with nodejs blueprint for : "$1
 
+componentGenerator="spring-"$2 $3
 runOptions="--blueprints nodejs --skip-git --skip-checks --force --no-insight --skip-install"
 
-if [ "$2" = "import-jdl" ]; then
-  runOptions="import-jdl "$1".jdl $runOptions"
-fi
+jhipster $componentGenerator $runOptions
 
-jhipster $runOptions
-
-if [ "$2" = "post-import-jdl" ]; then
-  echo "*** run import jdl after generation for : "$1
-  jhipster import-jdl $1.jdl $runOptions
-fi
-
-echo "*** check if the generation is wrong for some default java classes created :"
+echo "*** check if the "$2 "generation is wrong for some default java classes created :"
 
 if [ -z $(find src -type f -name "*.java" ) ]; then
       echo "${GREEN}GENERATION OK"
 else
       echo "${RED}WRONG GENERATION"
       exit 1
-fi
-
-echo "*** install client dependencies for : "$1
-sudo npm install
-if [ $? -ne 0 ]; then
-  echo "${RED}FAILED CLIENT INSTALL"
-  exit 1
-fi
-echo "*** install server dependencies for : "$1
-cd server && sudo npm install
-if [ $? -ne 0 ]; then
-  echo "${RED}FAILED SERVER INSTALL"
-  exit 1
 fi
