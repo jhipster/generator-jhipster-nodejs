6550419fa03fd3192ebd2cc50b289b6c85a93716
Angelo Manganiello <angelo.mang@libero.it>
2020-03-24 10:07:21 +0100

    coverage main app

--- test/app.spec.js
+++ test/app.spec.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const fse = require('fs-extra');
 const assert = require('yeoman-assert');
 const helpers = require('yeoman-test');
 const constants = require('../generators/generator-nodejs-constants');
@@ -14,8 +15,16 @@ const prompt = {
     clientTheme: 'none'
 };
 
+function commonAssertions() {
+    // assertion for client subgenerator
+    assert.fileContent('package.json', '"start:app": "npm run build && cd server && npm run start"');
+
+    // assertion for common subgenerator
+    assert.fileContent('README.md', 'https://github.com/jhipster/generator-jhipster-nodejs');
+}
+
 describe('Main app generator of nodejs JHipster blueprint', () => {
-    describe('1-test', () => {
+    describe('1-test full app', () => {
         before(done => {
             helpers
                 .run('generator-jhipster/generators/app')
@@ -39,14 +48,73 @@ describe('Main app generator of nodejs JHipster blueprint', () => {
         });
 
         it('app exists with server, client and common nodejs subgenerators', () => {
+            commonAssertions();
             // assertion for server subgenerator
             assert.file(`${SERVER_NODEJS_DIR}src/app.module.ts`);
+        });
+    });
 
-            // assertion for client subgenerator
-            assert.fileContent('package.json', '"start:app": "npm run build && cd server && npm run start"');
+    describe('2-test app skip server', () => {
+        before(done => {
+            helpers
+                .run('generator-jhipster/generators/app')
+                .withOptions({
+                    'from-cli': true,
+                    skipInstall: true,
+                    blueprints: 'nodejs',
+                    skipServer: true,
+                    db: 'sql',
+                    auth: 'jwt',
+                    skipChecks: true
+                })
+                .withGenerators([
+                    [
+                        require('../generators/app/index.js'), // eslint-disable-line global-require
+                        'jhipster-nodejs:app',
+                        path.join(__dirname, '../generators/app/index.js')
+                    ]
+                ])
+                .withPrompts({
+                    prompt
+                })
+                .on('end', done);
+        });
 
-            // assertion for common subgenerator
-            assert.fileContent('README.md', 'https://github.com/jhipster/generator-jhipster-nodejs');
+        it('app exists without server', () => {
+            commonAssertions();
+        });
+    });
+
+    describe('3-test app regeneration', () => {
+        before(done => {
+            helpers
+                .run('generator-jhipster/generators/app')
+                .inTmpDir(dir => {
+                    fse.copySync(path.join(__dirname, '../test/templates/ngx-blueprint'), dir);
+                })
+                .withOptions({
+                    'from-cli': true,
+                    skipInstall: true,
+                    blueprints: 'nodejs',
+                    skipChecks: true
+                })
+                .withGenerators([
+                    [
+                        require('../generators/app/index.js'), // eslint-disable-line global-require
+                        'jhipster-nodejs:app',
+                        path.join(__dirname, '../generators/app/index.js')
+                    ]
+                ])
+                .withPrompts({
+                    actionAdd: false
+                })
+                .on('end', done);
+        });
+
+        it('app regenerated exists with server, client and common nodejs', () => {
+            commonAssertions();
+            // assertion for server subgenerator
+            assert.file(`${SERVER_NODEJS_DIR}src/app.module.ts`);
         });
     });
 });
