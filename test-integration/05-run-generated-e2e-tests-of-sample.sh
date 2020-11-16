c447cf47e9dfc5cea596726ce5416174b7540b6e
Angelo Manganiello <angelo.mang@libero.it>
2020-11-16 17:54:26 +0100

    update release notes and shelljs version for warnings

--- test-integration/05-run-generated-e2e-tests-of-sample.sh
+++ test-integration/05-run-generated-e2e-tests-of-sample.sh
@@ -13,11 +13,19 @@ echo "*** changed directory in : test-integration/samples/"$1
 
 
 #-------------------------------------------------------------------------------
-# Run e2e test 
+# Run e2e test
 #-------------------------------------------------------------------------------
-echo "*** run e2e test in server for : "$1
+echo "*** run protractor e2e test in client for : "$1
+npm run e2e
+if [ $? -ne 0 ]; then
+  echo "${RED}FAILED PROTRACTOR CLIENT E2E TEST COMMAND"
+  exit 1
+fi
+
+
+echo "*** run jest e2e test in server for : "$1
 cd server && npm run test:e2e
 if [ $? -ne 0 ]; then
-  echo "${RED}FAILED SERVER E2E TEST COMMAND"
+  echo "${RED}FAILED JEST SERVER E2E TEST COMMAND"
   exit 1
 fi
