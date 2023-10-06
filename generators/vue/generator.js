7fdcbad89a7c04e7add7f36e564a6d757d62d579
Marcelo Shima <marceloshima@gmail.com>
2023-10-06 19:56:55 -0300

    disable eslint at vue

---
+++ generators/vue/generator.js
@@ -0,0 +1,19 @@
+import BaseApplicationGenerator from 'generator-jhipster/generators/base-application';
+
+export default class extends BaseApplicationGenerator {
+  constructor(args, opts, features) {
+    super(args, opts, { ...features, sbsBlueprint: true });
+  }
+
+  get [BaseApplicationGenerator.POST_WRITING]() {
+    return this.asPostWritingTaskGroup({
+      async postWritingTemplateTask() {
+        this.packageJson.merge({
+          scripts: {
+            pretest: undefined,
+          },
+        });
+      },
+    });
+  }
+}
