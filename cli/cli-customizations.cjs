568a78a8f50b3485cb4281b2b48653e8c8dfca00
Marcelo Shima <marceloshima@gmail.com>
2024-11-06 13:46:31 -0300

    Update cli/cli-customizations.cjs

--- cli/cli-customizations.cjs
+++ cli/cli-customizations.cjs
@@ -1,2 +1,9 @@
 // This file will not be overwritten by generate-blueprint
-module.exports = {};
+module.exports = {
+  printLogo: async () => {
+    const { getLogo } = await import('./logo.js');
+    const { version } = require('../package.json');
+    console.log(getLogo(version));
+  },
+  printBlueprintLogo: undefined,
+};
