a9261ff1046878117e3db56e2e7669689f7e0271
Marcelo Shima <marceloshima@gmail.com>
2024-11-06 14:48:06 -0300

    adjusts

---
+++ .blueprint/generate-sample/templates/angular.js
@@ -0,0 +1,38 @@
+export default {
+  'monolith-angular-jwt-jdl': {
+    'sample': 'monolith-client-auth-template-jdl',
+    'extra-args': '--auth jwt',
+    'sample-type': 'jdl',
+  },
+  'monolith-angular-jwt-i18n-jdl': {
+    'sample': 'monolith-client-auth-i18n-template-jdl',
+    'extra-args': '--auth jwt',
+    'sample-type': 'jdl',
+  },
+  'monolith-angular-oauth2-jdl': {
+    'sample': 'monolith-client-auth-template-jdl',
+    'extra-args': '--auth oauth2',
+    'sample-type': 'jdl',
+  },
+  'monolith-angular-oauth2-mongodb-jdl': {
+    'sample': 'monolith-angular-auth-mongodb-template-jdl',
+    'extra-args': '--auth oauth2',
+    'sample-type': 'jdl',
+  },
+  'monolith-angular-oauth2-i18n-jdl': {
+    'sample-type': 'jdl',
+  },
+  'monolith-angular-jwt-i18n-dev': {
+    'sample-type': 'yo-rc',
+  },
+  'monolith-angular-jwt-mongodb-jdl': {
+    'sample': 'monolith-angular-auth-mongodb-template-jdl',
+    'extra-args': '--auth jwt',
+  },
+  'monolith-angular-mssql-prod-jdl': {
+    'sample-type': 'jdl',
+  },
+  'monolith-angular-postgresql-prod-jdl': {
+    'sample-type': 'jdl',
+  },
+}
