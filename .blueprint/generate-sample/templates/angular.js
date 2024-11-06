c349e351bd0f0c5c42b9532bde12a0e33b6517ed
Marcelo Shima <marceloshima@gmail.com>
2024-11-06 15:31:34 -0300

    Update angular.js

--- .blueprint/generate-sample/templates/angular.js
+++ .blueprint/generate-sample/templates/angular.js
@@ -1,43 +1,47 @@
 export default {
   'monolith-angular-jwt-jdl': {
     'sample-file': 'monolith-client-auth-template-jdl',
-    'sample-folder': '../samples/',
+    'sample-folder': 'samples/',
     'extra-args': '--auth jwt',
     'sample-type': 'jdl',
   },
   'monolith-angular-jwt-i18n-jdl': {
     'sample-file': 'monolith-client-auth-i18n-template-jdl',
-    'sample-folder': '../samples/',
+    'sample-folder': 'samples/',
     'extra-args': '--auth jwt',
     'sample-type': 'jdl',
   },
   'monolith-angular-oauth2-jdl': {
     'sample-file': 'monolith-client-auth-template-jdl',
-    'sample-folder': '../samples/',
+    'sample-folder': 'samples/',
     'extra-args': '--auth oauth2',
     'sample-type': 'jdl',
   },
   'monolith-angular-oauth2-mongodb-jdl': {
     'sample-file': 'monolith-angular-auth-mongodb-template-jdl',
-    'sample-folder': '../samples/',
+    'sample-folder': 'samples/',
     'extra-args': '--auth oauth2',
     'sample-type': 'jdl',
   },
   'monolith-angular-oauth2-i18n-jdl': {
+    'sample-folder': 'samples/',
     'sample-type': 'jdl',
   },
   'monolith-angular-jwt-i18n-dev': {
+    'sample-folder': 'samples/',
     'sample-type': 'yo-rc',
   },
   'monolith-angular-jwt-mongodb-jdl': {
     'sample-file': 'monolith-angular-auth-mongodb-template-jdl',
-    'sample-folder': '../samples/',
+    'sample-folder': 'samples/',
     'extra-args': '--auth jwt',
   },
   'monolith-angular-mssql-prod-jdl': {
+    'sample-folder': 'samples/',
     'sample-type': 'jdl',
   },
   'monolith-angular-postgresql-prod-jdl': {
+    'sample-folder': 'samples/',
     'sample-type': 'jdl',
   },
 }
