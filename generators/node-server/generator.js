5161c6c59b6e0979acbcf4ad0cba50194932fded
Marcelo Shima <marceloshima@gmail.com>
2024-11-11 21:05:51 -0300

    adjusts to entity relations (#599)

--- generators/node-server/generator.js
+++ generators/node-server/generator.js
@@ -112,6 +112,7 @@ export default class extends BaseApplicationGenerator {
     return this.asPreparingTaskGroup({
       async preparing({ application }) {
         application.typeormOrderSupport = !application.databaseTypeMongodb;
+        application.typeormRelationsSupport = !application.databaseTypeMongodb;
       },
     });
   }
