893937554a6c15a7701be43a89cb725765ca86ee
Nourhan Elashamwy <nourhan_ashmawy@yahoo.com>
2026-06-14 11:38:29 +0300

    fix: generate pnpm workspace files

--- generators/node-server/files.js
+++ generators/node-server/files.js
@@ -5,7 +5,7 @@ const SERVER_NODEJS_DIR = `${SERVER_NODEJS_SRC_DIR}/`;
 export const serverFiles = {
   common: [
     {
-      templates: ['.dockerignore', 'README.md.jhi.nodejs', '.gitignore.jhi.nodejs', 'Dockerfile'],
+      templates: ['.dockerignore', 'README.md.jhi.nodejs', '.gitignore.jhi.nodejs', 'Dockerfile', '.npmrc', 'pnpm-workspace.yaml'],
     },
     {
       path: SERVER_NODEJS_DIR,
